49e204f0b18fea49e60357e4eca020931b513366
Utkarsh Pratap Singh <singhutkarsh297@gmail.com>
2026-08-12 08:24:37 +0530

    compleated program 33

--- src/Program33.java
+++ src/Program33.java
@@ -1,4 +1,22 @@
 // Write a program to count the number of digits in a number n
 
+import java.util.Scanner;
+
 public class Program33 {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter n: ");
+        int n = input.nextInt();
+
+        int count = 0;
+
+        while (n != 0) {
+            n = n / 10;
+            count++;
+        }
+
+        System.out.println("Number of digits = " + count);
+    }
 }
