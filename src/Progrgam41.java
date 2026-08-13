05acbd0be63497c3a2868bf730888550cff5d493
Utkarsh Pratap Singh <singhutkarsh297@gmail.com>
2026-08-13 16:19:13 +0530

    updated program 41

--- src/Progrgam41.java
+++ src/Progrgam41.java
@@ -1,4 +1,24 @@
-//
+//  Write a program to check whether a number n is a palindrome (reads the same reversed).
+
+import java.util.Scanner;
 
 public class Progrgam41 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the number : ");
+        int n = input.nextInt();
+        int original = n;
+        int reverse = 0;
+        while (n!=0){
+            int digit = n%10;
+            reverse = reverse * 10 +  digit ;
+            n /= 10;
+        }
+        if(original == reverse){
+            System.out.println("The number "+ original +" is Palindrome");
+        }
+        else{
+            System.out.println("The number "+ original +" is not Palindrome");
+        }
+    }
 }
