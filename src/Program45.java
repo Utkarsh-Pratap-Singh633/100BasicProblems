e58f9b58a2e1d02b325934f8cea97965919367d6
Utkarsh Pratap Singh <singhutkarsh297@gmail.com>
2026-08-13 18:57:32 +0530

    added  program 45

---
+++ src/Program45.java
@@ -0,0 +1,14 @@
+//  Write a program to display all prime numbers from 1 to n
+
+import java.util.Scanner;
+
+public class Program45 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number : ");
+        int n = sc.nextInt();
+        for (int i = 1; i <= n; i+=2) {
+            System.out.println(i);
+        }
+    }
+}
