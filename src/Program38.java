b56588112ed8cb331105df8e59d4ac6c663607dd
Utkarsh Pratap Singh <singhutkarsh297@gmail.com>
2026-08-13 13:55:46 +0530

    updated program 38

--- src/Program38.java
+++ src/Program38.java
@@ -7,10 +7,13 @@ public class Program38 {
         Scanner input = new Scanner(System.in);
         System.out.print("Please enter a number : ");
         int n = input.nextInt();
-        int largest = 1;
+        int largest = 0;
         while (n != 0) {
-            if (n>1)
-                largest = n/10;
+          int digit = n % 10;
+          if (digit > largest) {
+          largest = digit;
+          }
+          n = n / 10;
         }
         System.out.println("The largest number is :  " + largest);
     }
