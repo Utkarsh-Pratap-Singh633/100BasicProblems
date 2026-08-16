3bf1927ac557035a35a5fce38f24f80d37b34375
Utkarsh Pratap Singh <singhutkarsh297@gmail.com>
2026-08-16 22:26:53 +0530

    updated  program 51

--- src/Program51.java
+++ src/Program51.java
@@ -1,23 +1,43 @@
 // Write a program to check whether a number is an automorphic number.
 
+// Write a program to check whether a number is an automorphic number.
+
 import java.util.Scanner;
 
 public class Program51 {
-    static void main() {
+    public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter number : ");
+
+        System.out.print("Enter number: ");
         int n = sc.nextInt();
-        int mul = 1;
-        while (n != 0) {
-            mul = n*n;
-            int digit = n%10;
-            if(digit == mul)
-            {
-                System.out.println("It is automorphic number");
-            }
-            else{
-                System.out.println("It is not automorphic number");
-            }
+
+        int original = n;
+        int square = n * n;
+
+        int digits = 0;
+        int temp = n;
+
+        // Count the number of digits
+        while (temp != 0) {
+            digits++;
+            temp /= 10;
+        }
+
+        // Create 10, 100, 1000, etc.
+        int power = 1;
+
+        for (int i = 1; i <= digits; i++) {
+            power *= 10;
+        }
+
+        // Get the last digits of the square
+        int lastPart = square % power;
+
+        if (lastPart == original) {
+            System.out.println("It is an automorphic number");
+        } else {
+            System.out.println("It is not an automorphic number");
         }
     }
 }
