616ca2ec1f9e655a8e2f03e4bfcc9d5b2ddc300e
Utkarsh Pratap Singh <singhutkarsh297@gmail.com>
2026-08-20 19:21:45 +0530

    added program 63

--- src/Program63.java
+++ src/Program63.java
@@ -1,4 +1,19 @@
-//
+// Write a program to print a right-angled triangle pattern of stars of height n
+
+import java.util.Scanner;
 
 public class Program63 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number : ");
+        int n = sc.nextInt();
+
+        for(int row = 1; row <= n; row++){
+            for(int col = 1; col <= row; col++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+    }
 }
