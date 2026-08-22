5f58621b4de0b8b4a1262dbeb9921e0180603a54
Utkarsh Pratap Singh <singhutkarsh297@gmail.com>
2026-08-22 23:04:40 +0530

    added program 76

--- src/Program76.java
+++ src/Program76.java
@@ -1,2 +1,30 @@
+// Write a program to check whether two strings are anagrams of each other
+
+import java.util.Arrays;
+
 public class Program76 {
+    public static void main(String[] args) {
+
+        String str1 = "listen";
+        String str2 = "silent";
+
+        if (str1.length() != str2.length()) {
+            System.out.println("It is not anagrams");
+            return;
+        }
+
+        char[] arr1 = str1.toCharArray();
+        char[] arr2 = str2.toCharArray();
+
+        Arrays.sort(arr1);
+        Arrays.sort(arr2);
+
+        if (Arrays.equals(arr1, arr2)) {
+            System.out.println("It is an anagram");
+        }
+        else {
+            System.out.println("It is not an anagram");
+        }
+    }
 }
+
