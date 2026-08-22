e6759584f58de854b72abf1d6bad6e9fd96fdc44
Utkarsh Pratap Singh <singhutkarsh297@gmail.com>
2026-08-22 22:45:09 +0530

    added program 74

---
+++ src/Program74.java
@@ -0,0 +1,22 @@
+// Write a program to count the frequency of each character in a string.
+
+public class Program74 {
+    public static void main(String[] args) {
+
+        String str = "banana";
+
+        for (int i = 0; i < str.length(); i++) {
+
+            int count = 0;
+
+            for (int j = 0; j < str.length(); j++) {
+
+                if (str.charAt(i) == str.charAt(j)) {
+                    count++;
+                }
+            }
+
+            System.out.println(str.charAt(i) + " = " + count);
+        }
+    }
+}
