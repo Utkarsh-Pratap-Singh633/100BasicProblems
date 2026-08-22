5c2514b1dddde179872536be74929b60974745d4
Utkarsh Pratap Singh <singhutkarsh297@gmail.com>
2026-08-22 23:27:05 +0530

    added program 77

--- src/Program77.java
+++ src/Program77.java
@@ -1,2 +1,27 @@
+// Write a program to find the first non-repeating character in a string.
+
 public class Program77 {
+    public static void main(String[] args) {
+
+        String str = "Rajputana";
+
+        for (int i = 0; i < str.length(); i++) {
+
+            boolean isRepeating = false;
+
+            for (int j = 0; j < str.length(); j++) {
+
+                if (i != j && str.charAt(i) == str.charAt(j)) {
+                    isRepeating = true;
+                    break;
+                }
+            }
+
+            if (!isRepeating) {
+                System.out.println("First Non-Repeating Character: " + str.charAt(i));
+                break;
+            }
+        }
+    }
 }
+
