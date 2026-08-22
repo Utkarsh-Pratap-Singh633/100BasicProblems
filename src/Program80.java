265f3e4db4a1a938dffd4e5cf07d16095016c1ad
Utkarsh Pratap Singh <singhutkarsh297@gmail.com>
2026-08-22 23:42:14 +0530

    added program 80

---
+++ src/Program80.java
@@ -0,0 +1,10 @@
+// Write a program to read n elements into an array and print them
+
+public class Program80 {
+    public static void main(String[] args) {
+        int[] arr = {10, 20, 30, 40, 50, 60, 70, 80, 90, 100};
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+}
