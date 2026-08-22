af40a6278fd0983bfbebac20af6f387df5319e45
Utkarsh Pratap Singh <singhutkarsh297@gmail.com>
2026-08-22 23:31:46 +0530

    added program 78

---
+++ src/Program78.java
@@ -0,0 +1,11 @@
+// Write a program to replace all occurrences of a character with another character in a string.
+
+public class Program78 {
+    public static void main(String[] args) {
+        String str = "Raga";
+        str = str.replaceAll("R", "D");
+
+        System.out.println(str);
+    }
+}
+
