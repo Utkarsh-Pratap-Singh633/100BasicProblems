c7e5c994f4bc1f800787c350e5fb638885cd986f
Utkarsh Pratap Singh <singhutkarsh297@gmail.com>
2026-08-22 22:54:57 +0530

    updated program 75

--- src/Program75.java
+++ src/Program75.java
@@ -1,6 +1,6 @@
 // Write a program to remove all spaces from a string.
 
-public class Problem75 {
+public class Program75 {
     public static void main(String[] args) {
         String str = "jai ho bhole baba ki";
         str = str.replaceAll(" ", "");
